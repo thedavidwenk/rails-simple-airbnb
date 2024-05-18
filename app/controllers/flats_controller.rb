958909ed1534c401112438c2efca25065c1c0a6e
David Wenk <davidwenk@icloud.com>
2024-05-18 20:18:52 +0200

    add a flat capability

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,4 +2,20 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save!
+
+    redirect_to flats_path
+  end
+
+    # Only allow a list of trusted parameters through.
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
